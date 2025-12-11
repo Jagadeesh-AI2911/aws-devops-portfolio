18ec6583d2ff39a42d95c5628d7c52fe302b2f6d
Jagadeesh Vadithya <jagadeeshvadithya@MacBookAir.lan>
2025-12-11 11:49:38 -0500

    upgrade to High Availability: ALB + ASG

--- infrastructure/dev/main.tf
+++ infrastructure/dev/main.tf
@@ -2,15 +2,15 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# ----------------------
-# Networking Layer
-# ----------------------
+# -------------------------------------------------------------
+# 1. NETWORKING LAYER (VPC & Subnets)
+# -------------------------------------------------------------
 
-# 1. vpc
 resource "aws_vpc" "main" {
-  cidr_block            = "10.0.0.0/16"
-  enable_dns_support    = true
-  enable_dns_hostnames  = true
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
   tags = {
     Name = "portfolio-vpc"
     Environment = "dev"
@@ -18,31 +18,38 @@ resource "aws_vpc" "main" {
   }
 }
 
-# 2. IGW
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
-  tags = {
-    Name = "portfolio-igw"
-    Environment = "dev"
-    Project = "portfolio"
-  }
 }
 
-# 3. Public Subnet
-
-resource "aws_subnet" "Public" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+# Subnet 1 (Zone A)
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone = "us-east-1a"
-  tags = {
-    Name = "portfolio-public-subnet"
+  availability_zone       = "us-east-1a"
+
+  tags = { 
+    Name = "portfolio-public-subnet-1" 
     Environment = "dev"
     Project = "portfolio"
   }
 }
 
-# 4. Route Table
+# Subnet 2 (Zone B) - REQUIRED for Load Balancer High Availability
+resource "aws_subnet" "public_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.3.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1b"
+
+  tags = { 
+    Name = "portfolio-public-subnet-2" 
+    Environment = "dev"
+    Project = "portfolio"
+  }
+}
+
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
   route {
@@ -50,126 +57,204 @@ resource "aws_route_table" "public_rt" {
     gateway_id = aws_internet_gateway.igw.id
   }
   tags = {
-    Name = "portfolio-public-route"
+    Name = "portfolio-public-route-table"
     Environment = "dev"
     Project = "portfolio"
   }
 }
 
-# 5. Route Table Association
-
-resource "aws_route_table_association" "public_rt" {
-  subnet_id = aws_subnet.Public.id
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
   route_table_id = aws_route_table.public_rt.id
+  depends_on     = [aws_route_table.public_rt]
+  
 }
 
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public_rt.id
+  depends_on     = [aws_route_table.public_rt]
+}
 
-# -------------------------
-# Security & Private Layer
-# -------------------------
+# -------------------------------------------------------------
+# 2. SECURITY LAYER (Firewalls)
+# -------------------------------------------------------------
 
-# 1. Security Group: load balancer (public)
-resource "aws_security_group" "portfolio_alb_sg" {
-  name = "portfolio-sg"
-  description = "portfolio security group allo http inbound traffic"
-  vpc_id = aws_vpc.main.id
-  ingress {
-    description = "HTTP from internet"
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_security_group" "alb_sg" {
+  name        = "portfolio-alb-sg"
+  description = "Allow HTTP from Anywhere"
+  vpc_id      = aws_vpc.main.id
+  depends_on  = [aws_vpc.main]
   tags = {
-    Name = "portfolio-sg"
+    Name = "portfolio-alb-sg"
     Environment = "dev"
     Project = "portfolio"
   }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-# 2. private subnet
-resource "aws_subnet" "private" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
-  availability_zone = "us-east-1a"
+resource "aws_security_group" "asg_sg" {
+  name        = "portfolio-asg-sg"
+  description = "Allow HTTP from ALB only"
+  vpc_id      = aws_vpc.main.id
+  depends_on  = [aws_vpc.main]
   tags = {
-    Name = "portfolio-private-subnet"
+    Name = "portfolio-asg-sg"
     Environment = "dev"
     Project = "portfolio"
   }
-}
 
-# 3. security group: ec2 (private)
-resource "aws_security_group" "app_sg" {
-  name = "portfolio-app-sg"
-  description = "portfolio security group allow traffic from ALB only"
-  vpc_id = aws_vpc.main.id
   ingress {
-    description = "HTTP from ALB"
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    security_groups = [aws_security_group.portfolio_alb_sg.id] # allow traffic from ALB only
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
   }
+
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-    description = "allow all outbound"
   }
 }
 
 # -------------------------------------------------------------
-# The Web Server (Compute Layer)
+# 3. COMPUTE LAYER (Launch Template & ASG)
 # -------------------------------------------------------------
 
-
-# 9. Dynamic AMI Lookup (Get latest Amazon Linux 2023)
 data "aws_ami" "amazon_linux" {
   most_recent = true
   owners      = ["amazon"]
-
   filter {
     name   = "name"
     values = ["al2023-ami-2023.*-x86_64"]
   }
+  tags = {
+    Name = "Lates-AMI"
+    Environment = "dev"
+    Project = "portfolio"
+  }
 }
 
-# 10. The Web Server Instance
-resource "aws_instance" "web_server" {
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = "t2.micro" 
-  subnet_id     = aws_subnet.Public.id
+# The "Blueprint" for our servers
+resource "aws_launch_template" "app_lt" {
+  name_prefix   = "portfolio-app-"
+  image_id      = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
 
-  # Security Group: We use the ALB SG for now because it allows HTTP from Anywhere.
-  # In a later step (Day 4), we will move this to the App SG when we add a Load Balancer.
-  vpc_security_group_ids = [aws_security_group.portfolio_alb_sg.id]
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups             = [aws_security_group.asg_sg.id]
+  }
 
-  # User Data: This script runs ONLY once when the instance starts.
-  # It installs Nginx and creates a custom HTML page.
-  user_data = <<-EOF
+  # Installs Nginx and creates a dynamic HTML page with the server's ID
+  user_data = base64encode(<<-EOF
               #!/bin/bash
               dnf update -y
               dnf install -y nginx
               systemctl start nginx
               systemctl enable nginx
-              echo "<h1>Deployed via Terraform</h1><p>Welcome to Jagadeesh's Portfolio Project!</p>" > /usr/share/nginx/html/index.html
+              INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+              echo "<h1>High Availability Cluster</h1><p>Served by instance: $INSTANCE_ID</p>" > /usr/share/nginx/html/index.html
               EOF
-
+  )
   tags = {
-    Name = "portfolio-web-server"
+    Name = "portfolio-app-lt"
+    Environment = "dev"
+    Project = "portfolio"
   }
 }
 
-# 11. Output the URL (So you can click it easily)
-output "web_server_url" {
-  value       = "http://${aws_instance.web_server.public_ip}"
-  description = "Click the URL to see deployed website"
+# The "Manager" that ensures 2 servers are always running
+resource "aws_autoscaling_group" "app_asg" {
+  desired_capacity    = 2
+  max_size            = 3
+  min_size            = 1
+  vpc_zone_identifier = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+  target_group_arns   = [aws_lb_target_group.app_tg.arn]
+
+  launch_template {
+    id      = aws_launch_template.app_lt.id
+    version = "$Latest"
+  }
+  
+  # Wait for instance to be healthy before considering it "Done"
+  health_check_type         = "ELB"
+  health_check_grace_period = 300
+  default_cooldown          = 10
+}
+
+# -------------------------------------------------------------
+# 4. LOAD BALANCING LAYER (ALB)
+# -------------------------------------------------------------
+
+resource "aws_lb" "app_alb" {
+  name               = "portfolio-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+  depends_on         = [aws_security_group.alb_sg]
+  tags = {
+    Name = "portfolio-alb"
+    Environment = "dev"
+    Project = "portfolio"
+  }
+}
+
+resource "aws_lb_target_group" "app_tg" {
+  name     = "portfolio-app-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+  health_check {
+    path                = "/"
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+  depends_on = [aws_lb.app_alb]
+  tags = {
+    Name = "portfolio-app-tg"
+    Environment = "dev"
+    Project = "portfolio"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+  depends_on = [aws_lb_target_group.app_tg]
+  tags = {
+    Name = "portfolio-alb-listener"
+    Environment = "dev"
+    Project = "portfolio"
+  }
+}
+
+# -------------------------------------------------------------
+# 5. OUTPUTS
+# -------------------------------------------------------------
+output "alb_dns_name" {
+  value       = "http://${aws_lb.app_alb.dns_name}"
+  description = "The Public URL of your Load Balancer"
 }
