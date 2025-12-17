64b1f63f6d4fcf467d7cf922af4e8c077bb6f2e4
Jagadeesh Vadithya <jagadeeshvadithya@MacBookAir.lan>
2025-12-17 15:57:58 -0500

    Add outputs.tf to live environments

---
+++ infrastructure/modules/portfolio-app/infrastructure/live/prod/outputs.tf
@@ -0,0 +1,3 @@
+output "alb_url" {
+  value = modules.portfolio-app.alb_dns_name
+}
--- infrastructure/modules/portfolio-app/outputs.tf
+++ infrastructure/modules/portfolio-app/outputs.tf
@@ -1,19 +1,13 @@
 output "alb_dns_name" {
   description = "The Public URL of the Load Balancer"
-  # If ALB was created, return DNS. If not, return "Not Created"
-  value = var.enable_alb ? "http://${aws_lb.app[0].dns_name}" : "ALB Disabled (Cost Saving)"
+  value = var.enable_alb ? "http://${aws_lb.app[0].dns_name}" : "ALB Disabled"
 }
 
 output "db_endpoint" {
   description = "The Private Endpoint of the Database"
-  value       = var.instance_count > 0 ? aws_db_instance.default[0].endpoint : "Database Disabled (Cost Saving)"
+  value       = var.instance_count > 0 ? aws_db_instance.default[0].endpoint : "Database Disabled"
 }
 
 output "vpc_id" {
   value = aws_vpc.main.id
-}
-
-output "alb_url" {
-  description = "The Public URL of the Load Balancer"
-  value       = module.portfolio-app.alb_dns_name
 }
